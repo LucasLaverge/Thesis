ff52ca914d3800c1625698569dd74a3276a9784a
LucasLaverge <lucas.laverge@gmail.com>
2023-03-13 16:56:00 +0100

    Lot's of data cleaning aditions

--- DataCleaning.R
+++ DataCleaning.R
@@ -1,15 +1,77 @@
 #packages
 library(readr)
+library(tidyverse)
+library(lubridate)
 
 
 ############################################################################
 ########################## LOAD DATA #######################################
 ############################################################################
 # Load and read data -------------------------------------------------------
-# dataset
-DataSet <- read_csv("DataSet.csv")
-View(DataSet)
+  # datasets (with choice text and with numeric variables)
+  df1 <- read_csv("DataSet.csv")
+  df2 <- read_csv("DataSet2.csv")
+  # Remove row 2 and 3 that contain import ID and question name
+  df1 <- df1[-c(1,2),]
+  df2 <- df2[-c(1,2),]
+  # Delete columns 
+  df1 <- df1[,c(1:25)] # Delete columns of last three parts in first dataset
+  df2 <- df2[,-c(1:8,10:25)] # Delete all columns expect those of last three parts in second dataset
+  
+# Merge both data sets ------------------------------------------------------
+  # Create merged df, using response id
+  df <- merge(df1, df2, by = "ResponseId")
+  # Remove df1 and df2
+  remove(df1, df2)
+  # Bring variable names tolower and change character types
+  df<- df %>% 
+    rename_with(tolower) %>% 
+    mutate(startdate = lubridate::ymd_hms(startdate),
+           enddate = lubridate::ymd_hms(enddate)) %>% 
+    mutate(
+      q4.2 = as.numeric(q4.2), q4.3 = as.numeric(q4.3), q4.4 = as.numeric(q4.4), q4.5 = as.numeric(q4.5), 
+      q4.6 = as.numeric(q4.6), q4.7 = as.numeric(q4.7), q4.8 = as.numeric(q4.8), q4.9 = as.numeric(q4.9), 
+      q4.10 = as.numeric(q4.10), q4.11 = as.numeric(q4.11), q4.12 = as.numeric(q4.12), q4.13 = as.numeric(q4.13), 
+      q4.14 = as.numeric(q4.14), q4.15 = as.numeric(q4.15), q4.16 = as.numeric(q4.16), q4.17 = as.numeric(q4.17), 
+      q4.18 = as.numeric(q4.18), q4.19 = as.numeric(q4.19), q4.20 = as.numeric(q4.20), q4.21 = as.numeric(q4.21), 
+      q4.22 = as.numeric(q4.22)
+    ) %>% 
+    mutate(
+      q5.2 = as.numeric(q5.2), q5.3 = as.numeric(q5.3), q5.4 = as.numeric(q5.4), q5.5 = as.numeric(q5.5), 
+      q5.6 = as.numeric(q5.6), q5.7 = as.numeric(q5.7), q5.8 = as.numeric(q5.8), q5.9 = as.numeric(q5.9), 
+      q5.10 = as.numeric(q5.10), q5.11 = as.numeric(q5.11), q5.12 = as.numeric(q5.12), q5.13 = as.numeric(q5.13), 
+      q5.14 = as.numeric(q5.14), q5.15 = as.numeric(q5.15), q5.16 = as.numeric(q5.16), q5.17 = as.numeric(q5.17), 
+      q5.18 = as.numeric(q5.18), q5.19 = as.numeric(q5.19), q5.20 = as.numeric(q5.20), q5.21 = as.numeric(q5.21), 
+      q5.22 = as.numeric(q5.22), q5.23 = as.numeric(q5.23), q5.24 = as.numeric(q5.24), q5.25 = as.numeric(q5.25), 
+      q5.26 = as.numeric(q5.26), q5.27 = as.numeric(q5.27), q5.28 = as.numeric(q5.28)
+    ) %>% 
+    mutate(
+      q6.2 = as.numeric(q6.2), q6.3 = as.numeric(q6.3), q6.4 = as.numeric(q6.4), q6.5 = as.numeric(q6.5),
+      q6.6 = as.numeric(q6.6), q6.7 = as.numeric(q6.7), q6.8 = as.numeric(q6.8), q6.9 = as.numeric(q6.9),
+      q6.10 = as.numeric(q6.10), q6.11 = as.numeric(q6.11), q6.12 = as.numeric(q6.12), q6.13 = as.numeric(q6.13)
+    ) %>% 
+    mutate(
+      sc0 = as.numeric(sc0), sc1 = as.numeric(sc1), sc2 = as.numeric(sc2)
+    )
 
+
+
+# Remove useless columns
+
+
+# Convert character columns to numeric variables
+class(df$sc1)
+df <- df %>% 
+  mutate(
+    sc1 = as.numeric(df$sc1)
+  )
+
+
+
+
+#bin product culture scores
+score_groups = cut(df$SC1, breaks = c(70,82,94,106,118,130,142,154,166,178,190,202,214,226), 
+                   labels = c("bin1", "bin2", "bin3", "bin4", "bin5", "bin6", "bin7", "bin8", "bin9", "bin10", "bin11", "bin12", "bin13"))
 
 
 
