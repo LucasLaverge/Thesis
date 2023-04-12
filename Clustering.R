8d6208764c5e7719efc5c11f529495857466f547
LucasLaverge <lucas.laverge@gmail.com>
2023-04-12 15:20:17 +0200

    Adapt code: - use initial.centers for the k means clustering

--- Clustering.R
+++ Clustering.R
@@ -11,8 +11,8 @@ library(GPArotation)
 # Create the plot
   ggplot(df, aes(innovation.score, execution.score)) +
     geom_point() + # Add points
-    scale_x_continuous(limits = c(0, 120)) + # Set the x-axis limits
-    scale_y_continuous(limits = c(0, 120)) # Set the y-axis limits
+    scale_x_continuous(limits = c(0.5, 1)) + # Set the x-axis limits
+    scale_y_continuous(limits = c(0.5, 1)) # Set the y-axis limits
   # Plot scores 
   plot(df$innovation.score, df$execution.score)
 
@@ -21,8 +21,12 @@ library(GPArotation)
   vars <- c("innovation.score", "execution.score")
   dataset <- df[vars]
   # Run k-means clustering with k=4
-  set.seed(12345)
-  kmeans.result <- kmeans(dataset, centers = 4)
+  # set.seed(12345)
+  initial.centres <- matrix(c(1, 1,0.5, 0.5,
+                              1,0.5,1, 0.5), ncol = 2)
+  kmeans.result <- kmeans(dataset, centers = initial.centres) # with initial.centers
+   # kmeans.result <- kmeans(dataset, centers = 4) # with set.seed
+  
   # View the cluster assignments
   kmeans.result$cluster
   # View the centroids
@@ -36,6 +40,11 @@ library(GPArotation)
                color = "black", size = 5, shape = 21) +
     ggtitle("K-means Clustering Results") +
     labs(x = "Innovation Score", y = "Execution Score")
+  
+  # Get number of iterations
+  num_iterations <- kmeans.result$iter
+  print(num_iterations)
+  
   # Remove
   remove(vars, dataset, kmeans.result)
   summary(df$execution.score)
