e97607e265a6ba0a181febeb1c0651a6a0ad4c6b
LucasLaverge <lucas.laverge@gmail.com>
2023-04-21 14:44:37 +0200

    EFA

--- EFA.R
+++ EFA.R
@@ -5,12 +5,35 @@ library(GPArotation)
 
 # Exploratory Factor Analysis of Execution culture ------------------------------
 # select the variables for EFA
-vars <- df[, paste0("q5.", 2:28)]
+vars <- df[, paste0("E.", 1:27)]
 
-# perform EFA with 2 factors
-efa.result <- fa(vars, nfactors = 6)
 
-# view the EFA results
-print(efa.result, sort=TRUE)
+# Do KMO and Barlett's test
+  # Barlett's test
+  bartlett <- bartlett.test(vars)
+  print(bartlett$p.value)
+  
+  # KMO test
+  kmo <- KMO(vars)
+  print(kmo$MSA)
+  print(kmo)
 
-#---------------------------------------------------------------------------------
+  
+# EFA
+  # Scree plot
+  fa.parallel(vars, fm = "ml", fa = "fa")
+  # EFA
+  efa <- fa(vars, nfactors = 7, rotate = "varimax")
+  ?fa
+  # Print total variance explained
+  efa$Vaccounted
+
+# Print rotated component matrix
+    print(efa$loadings,cutoff = 0.4) 
+
+
+# Deleting case with cross-loadings
+
+
+# Coefficient alphas
+
