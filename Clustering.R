4fcb72c065eccecd04e5e93b7d9b8eda1ba9007d
LucasLaverge <lucas.laverge@gmail.com>
2023-05-13 18:40:34 +0200

    Try to adapt cluster graph

--- Clustering.R
+++ Clustering.R
@@ -58,7 +58,6 @@ remove(model1, map_size_to_category, formula, xrange)
                color = "black", size = 4, shape = 21) +
     ggtitle("K-means Clustering Results") +
     labs(x = "Innovation Score", y = "Execution Score")
-
   #-------------------------------------------------------- 
   summary(df$execution.score)
   summary(df$innovation.score)
